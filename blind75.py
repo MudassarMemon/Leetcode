abf747410827454812735427bfcb8a1c7eb4eaf1
Mudassar Memon <128174228+MudassarMemon@users.noreply.github.com>
2023-11-13 15:57:35 -0500

    Complete 143

--- blind75.py
+++ blind75.py
@@ -836,3 +836,49 @@ class Solution(object):
             tail.next = list2
 
         return res.next
+
+# 143.
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+
+        second = slow.next
+        slow.next = None
+        prev = None
+
+        while second:
+            nxt = second.next
+            second.next = prev
+            prev = second
+            second = nxt
+
+        first = head
+        second = prev
+
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
+
+
+        
+        
+        
