5005f5eb9ab188d3cab13b4a75896b76e716e7ce
Mudassar Memon <128174228+MudassarMemon@users.noreply.github.com>
2023-11-13 14:08:44 -0500

    Complete 206

--- blind75.py
+++ blind75.py
@@ -776,3 +776,35 @@ class Solution(object):
         :rtype: bool
         """
         
+# 206. recursive
+
+class Solution(object):
+    def reverseList(self, head):
+
+        if not head:
+            return None
+
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+            head.next = None
+        else:
+            return head
+
+        return newHead
+
+# 206. Iterative:
+
+class Solution(object):
+    def reverseList(self, head):
+        prev, curr = None, head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        return prev
+
+
