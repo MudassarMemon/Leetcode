8a1a80e70f75e04894f86b075c2f20b00c8c16ad
Mudassar Memon <128174228+MudassarMemon@users.noreply.github.com>
2023-11-13 14:26:40 -0500

    Complete 21

--- blind75.py
+++ blind75.py
@@ -807,4 +807,32 @@ class Solution(object):
 
         return prev
 
+# 21. 
 
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        res = ListNode()
+        tail = res
+        
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+
+        return res.next
