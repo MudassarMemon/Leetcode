38488acbf029c30fb803db670d2caa437ab92060
Mudassar Memon <128174228+MudassarMemon@users.noreply.github.com>
2023-11-14 19:07:18 -0500

    Complete 100

--- blind75.py
+++ blind75.py
@@ -1142,4 +1142,28 @@ class Solution(object):
                 stack.append([node.right, depth + 1])
 
         return res
-            
+    
+# 100. Recursive DFS
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        
+        if not p or not q or p.val != q.val:
+            return False
+
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        
