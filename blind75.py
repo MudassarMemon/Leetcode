3c623614ea5fa6c67f8e575eabee29fd4437272f
Mudassar Memon <128174228+MudassarMemon@users.noreply.github.com>
2023-11-20 16:43:40 -0500

    Complete 102

--- blind75.py
+++ blind75.py
@@ -1230,4 +1230,40 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
+
+# 102.
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+
+        res = []
+
+        queue = collections.deque()
+        queue.append(root)
+
+        while queue:
+            qLen = len(queue)
+            level = []
+
+            for i in range(qLen):
+                node = queue.popleft()
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+                
+            if level:
+                res.append(level)
+
+        return res
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
         
