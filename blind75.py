12a7731e6a9ef5bdba4b925cebf2372652665a80
Mudassar Memon <128174228+MudassarMemon@users.noreply.github.com>
2023-11-21 14:43:51 -0500

    Complete optimized and iterative solution for 230

--- blind75.py
+++ blind75.py
@@ -1321,4 +1321,68 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+
+# 230. Optimized fewer recursive calls
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+
+        def makeList(node):
+            if not node:
+                return []
+
+            
+            return (makeList(node.left) if node.left else []) + [node.val] + (makeList(node.right) if node.right else [])
+
+
+        return makeList(root)[k-1]
+            
+
+
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+
+# 230 iterative solution
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        n = 0
+        stack = []
+        cur = root
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+
+            cur = stack.pop()
+            n += 1
+            
+            if k == n:
+                return cur.val
+            
+            cur = cur.right
+
+        
+
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
         
